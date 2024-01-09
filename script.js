569a4dd2cd1e6c930671a9c61967c52397dfe3e6
Saci <hs.sarahabo@gmail.com>
2024-01-09 20:53:44 +0100

    readingTime function added

---
+++ script.js
@@ -0,0 +1,23 @@
+/*function readingTime() {
+    const text = document.getElementById("article").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time").innerText = time;
+}
+
+window.onload = (function () { readingTime() });*/
+
+function readingTime() {
+    const text = document.getElementById("article").innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    document.getElementById("time").innerText = time;
+}
+
+if (document.readyState !== "loading") {
+    readingTime();
+} else {
+    document.addEventListener("DOMContentLoaded", readingTime);
+}
