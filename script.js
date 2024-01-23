f4cbeed2706577c4cff01299bc3e9f77201700c5
Saci <hs.sarahabo@gmail.com>
2024-01-23 22:25:18 +0100

    highlighting input text

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ if (document.readyState !== "loading") {
     });
 }*/
 
-document.addEventListener("DOMContentLoaded", function () {
+/*document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('search').addEventListener('input', function () {
         const searchValue = this.value.trim();
         const content = document.querySelector('main');
@@ -63,4 +63,75 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     })
-});
+});*/
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('search').addEventListener('input', function () {
+        var searchTerm = this.value.toLowerCase();
+        var articleContent = document.getElementById('article').innerHTML;
+
+        // Check if the search term is not empty
+        if (searchTerm !== "") {
+            // Use the RegExp constructor to create a dynamic regex pattern
+            var regex = new RegExp('(' + searchTerm + ')', 'ig');
+
+            // Use the replace method with a callback function to highlight search results
+            var highlightedContent = articleContent.replace(regex, function (match) {
+                return '<span class="highlight">' + match + '</span>';
+            });
+
+            // Update the article content with the highlighted version
+            document.getElementById('article').innerHTML = highlightedContent;
+        } else {
+            // If the search term is empty, revert to the original content
+            document.getElementById('article').innerHTML = articleContent;
+        }
+    })
+});*/
+
+/*function highlight(text) {
+    var inputText = document.getElementById("inputText");
+    var innerHTML = inputText.innerHTML;
+    var index = innerHTML.indexOf(text);
+    if (index >= 0) {
+        innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+        inputText.innerHTML = innerHTML;
+    }
+}*/
+
+
+
+window.onload = function () {
+    // Create an instance of mark.js and pass an argument containing
+    // the DOM object of the context (where to search for matches)
+    var markInstance = new Mark(document.querySelector("main"));
+    // Cache DOM elements
+    var keywordInput = document.querySelector("input[name='search']");
+    var optionInputs = document.querySelectorAll("input[name='opt[]']");
+
+    function performMark() {
+
+        // Read the keyword
+        var keyword = keywordInput.value;
+
+        // Determine selected options
+        var options = {};
+        /*[].forEach.call(optionInputs, function(opt) {
+          options[opt.value] = opt.checked;
+        });*/
+
+        // Remove previous marked elements and mark
+        // the new keyword inside the context
+        markInstance.unmark({
+            done: function () {
+                markInstance.mark(keyword, options);
+            }
+        });
+    };
+
+    // Listen to input and option changes
+    keywordInput.addEventListener("input", performMark);
+    for (var i = 0; i < optionInputs.length; i++) {
+        optionInputs[i].addEventListener("change", performMark);
+    }
+}
